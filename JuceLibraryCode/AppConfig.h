6299dd84fe3426f958f918f501e91a08a78f681f
Lopiano <John.m.l.caplan@gmail.com>
2015-12-17 16:30:55 -0600

    Sorted out some of the channels nonsense

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -331,14 +331,5 @@
 #ifndef  JucePlugin_AAXDisableMultiMono
  #define JucePlugin_AAXDisableMultiMono    0
 #endif
-#ifndef  JucePlugin_MaxNumInputChannels
- #define JucePlugin_MaxNumInputChannels    2
-#endif
-#ifndef  JucePlugin_MaxNumOutputChannels
- #define JucePlugin_MaxNumOutputChannels   2
-#endif
-#ifndef  JucePlugin_PreferredChannelConfigurations
- #define JucePlugin_PreferredChannelConfigurations  {1, 1}, {2, 2}
-#endif
 
 #endif  // __JUCE_APPCONFIG_MGUZ4F__
