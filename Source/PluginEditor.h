ad4b85c36018987a0d3b2d2ff61815d39ad811d7
Lopiano <John.m.l.caplan@gmail.com>
2015-12-24 17:12:24 -0600

    delete prev changes nvrmd

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -23,7 +23,6 @@ private Timer
 {
 public:
     WavelandSynthAudioProcessorEditor (WavelandSynthAudioProcessor&);
-    WavelandSynthAudioProcessorEditor ();
     ~WavelandSynthAudioProcessorEditor();
     
     //==============================================================================
